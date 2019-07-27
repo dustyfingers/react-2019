9fada6c2fd9d9c93b6014f4dd46ea112d9087699
dustyfingers <dustyfingers01257@gmail.com>
2019-07-27 16:15:51 -0600

    built cart dropdown component and styled it, added redux to show and hide dropdown, added cart to root reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,10 @@ import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
-const Header = ({ currentUser }) => (
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -22,11 +25,13 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
